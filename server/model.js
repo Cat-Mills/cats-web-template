ef19f34f2422850477bbe79c0df7b87a23c71c22
Cat-Mills <141058467+Cat-Mills@users.noreply.github.com>
2023-11-13 13:51:12 -0700

    started data models

--- server/model.js
+++ server/model.js
@@ -37,6 +37,77 @@ User.init(
   }
 );
 
+class Product extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+}
+
+Product.init(
+  {
+    productId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    productType: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    productPrice: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    productDesc: {
+      type: DataTypes.STRING
+    },
+    productNotes: {
+      type: DataTypes.STRING
+    }
+  },
+  {
+    modelName:"product",
+    sequelize: db
+  }
+)
+
+class Hero extends Model {
+  [util.inspect.custom]() {
+    return this.toJSON();
+  }
+}
+Hero.init(
+  {
+    heroId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    heroUrl: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    heroCTA: {
+      type: DataTypes.STRING,
+    },
+    heroLink: {
+      type: DataTypes.STRING,
+    },
+    heroButton: {
+      type: DataTypes.STRING
+    }
+  },
+  {
+    modelName: "hero",
+    sequelize: db
+  }
+)
+
 
 if (process.argv[1] === url.fileURLToPath(import.meta.url)) {
   console.log("Syncing to database...");
